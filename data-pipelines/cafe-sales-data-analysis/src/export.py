36db40208f7a534326c2adde1d843b56413b2730
Yassine amri <143635918+yassine10tn@users.noreply.github.com>
2026-03-27 22:07:13 +0100

    Change CSV file path to relative
    Updated the file path for reading the CSV to a relative path.

--- data-pipelines/cafe-sales-data-analysis/src/export.py
+++ data-pipelines/cafe-sales-data-analysis/src/export.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-df = pd.read_csv("C:\\Users\\Yassine\\Desktop\\projet python cafe sales\\data\\raw\\dirty_cafe_sales.csv", sep=";")
+df = pd.read_csv("data/raw/dirty_cafe_sales.csv", sep=";")
 
 df.replace(["ERROR", "UNKNOWN", ""], pd.NA, inplace=True)
 df["Total Spent"] = pd.to_numeric(df["Total Spent"], errors="coerce")
